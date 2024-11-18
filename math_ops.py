f96061366d43fdd698c7a2d07bd5dc30ab566adf
Zach Kaiser <zachwkaiser@gmail.com>
2024-11-17 20:33:12 -0500

    Added multiplication function

--- math_ops.py
+++ math_ops.py
@@ -6,9 +6,14 @@ def divide_numbers(a, b):
         return "Cannot divide by 0."
     return a / b  # Potential division by zero error
 
+def multiply_numbers(a, b):
+    """Multiplies two numbers"""
 
+    return a * b
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
+    mult_result = multiply_numbers(x, y)
     print(f"The result of division is: {result}")
+    print(f"The result of multiplication is: {mult_result}")
